b67f66156d3f66163316970c71513a107d757cc6
ammercu22 <73846405+ammercu22@users.noreply.github.com>
2024-02-17 06:55:22 -0600

    Added Compressed Texture files

--- src/Loader.jsx
+++ src/Loader.jsx
@@ -1,5 +1,7 @@
-import {Html} from '@react-three/drei'
+import {Html, useProgress} from '@react-three/drei'
 
 export default function Loader(){
+    const {progress , item} = useProgress()
+   
     return <Html center> Loading... </Html>
 }
